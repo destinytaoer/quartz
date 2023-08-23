7beb59a23377fb55bb184c1da9d20e3480027bcc
destiny <1848765519@qq.com>
2023-08-24 01:03:38 +0800

    chore: remove something about quartz

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "旬山🌱数字花园",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "xunshan.vip",
     ignorePatterns: ["private", "templates"],
     theme: {
       typography: {
